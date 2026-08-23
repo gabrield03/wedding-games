ff38039c48c254d6d262b591adf25152f295c4c8
Gabriel Larot <112510813+gabrield03@users.noreply.github.com>
2026-08-22 23:14:27 -0700

    Make Strands test fixture path-unique

--- tests/fixtures/strands.ts
+++ tests/fixtures/strands.ts
@@ -6,12 +6,12 @@ export const testStrandsPuzzle: StrandsPuzzle = {
   grid: {
     rows: 8,
     columns: 6,
-    letters: "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPVUTSRQ",
+    letters: "ABCDEFGHIJKLMNOPQZSTUVWXYZABCDEFGHIJKLMNOPVUTSRQ",
   },
   themeWords: [
     { word: "ABCDEF", path: [0, 1, 2, 3, 4, 5] },
     { word: "GHIJKL", path: [6, 7, 8, 9, 10, 11] },
-    { word: "MNOPQR", path: [12, 13, 14, 15, 16, 17] },
+    { word: "MNOPQZ", path: [12, 13, 14, 15, 16, 17] },
     { word: "STUVWX", path: [18, 19, 20, 21, 22, 23] },
     { word: "YZABCD", path: [24, 25, 26, 27, 28, 29] },
     { word: "EFGHIJ", path: [30, 31, 32, 33, 34, 35] },
